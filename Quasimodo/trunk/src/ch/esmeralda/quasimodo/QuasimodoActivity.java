c0be08bef652623d52ae45b5d956b567d2a1d64d
smach1337 <smach1337@46f43c3b-6de7-3a5c-3d44-7744824c1714>
2012-02-18 16:00:48 +0000

    Created base structures for Android project

---
+++ Quasimodo/trunk/src/ch/esmeralda/quasimodo/QuasimodoActivity.java
@@ -0,0 +1,13 @@
+package ch.esmeralda.quasimodo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuasimodoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
