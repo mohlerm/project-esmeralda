9dc3cc6945652604466efcf0362de86368af20a6
thomasrichner@gmail.com <thomasrichner@gmail.com@46f43c3b-6de7-3a5c-3d44-7744824c1714>
2012-02-22 12:01:58 +0000

    fixed main

--- NotreDame/trunk/src/ch/esmeralda/notredame/main/Main.java
+++ NotreDame/trunk/src/ch/esmeralda/notredame/main/Main.java
@@ -1,9 +1,11 @@
 package ch.esmeralda.notredame.main;
 
+import java.util.Date;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import ch.esmeralda.notredame.jobs.*;
+import ch.esmeralda.notredame.unitHandling.TaskUnit;
 import ch.esmeralda.notredame.unitHandling.Workday;
 import ch.esmeralda.notredame.unitHandling.WorkdayImpl;
 
@@ -18,23 +20,44 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("starting...");
 		// TODO Auto-generated method stub
 		ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(2);
 		
 		StreamJob streamJob = new StreamJobImpl();
+		
 		Workday workday = new WorkdayImpl();
-		//fill the workday with stuff
+		
+		if(args.length>0){
+			prefill(workday);
+			System.out.println(workday.toString());
+		}
+		
 		TimerJob timerJob = new TimerJobImpl(streamJob,workday);
 		
+		System.out.println("schedule jobs");
 		executor.scheduleAtFixedRate(timerJob, 500, 1000, TimeUnit.MILLISECONDS);
+		streamJob.start();
 		
 		try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {}
 		
+		System.out.println("stopping jobs");
 		executor.shutdownNow();
 		streamJob.stop(); // better way??
 		
+		
+		System.out.println("...bye, bye");
+	}
+	
+	private static void prefill(Workday workday){
+		long now = System.currentTimeMillis()+10000;
+		TaskUnit task;
+		for(int i=0;i<10;i++){
+			task = new TaskUnit(new Date(now+i*10000), 10000, "hallo");
+			workday.addUnit(task);
+		}
 	}
 
 }
