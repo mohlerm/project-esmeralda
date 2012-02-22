bdc8730d67f33c2fd722612ff27901150a2261b9
sandro.sgier <sandro.sgier@gmail.com>
2012-02-22 12:40:33 +0000

    changed StreamJob to interface, as otherwise it wouldn't make much sense....

--- NotreDame/trunk/src/ch/esmeralda/notredame/jobs/StreamJob.java
+++ NotreDame/trunk/src/ch/esmeralda/notredame/jobs/StreamJob.java
@@ -1,13 +1,12 @@
 package ch.esmeralda.notredame.jobs;
 
-
-public abstract class StreamJob extends Thread {
+public interface StreamJob {
 	/**
 	 * plays a stream from the given url or
 	 * plays the alarm if url is NULL
 	 * @param url an url of a stream
 	 */
-	public abstract void startStream(String url);
-	public abstract void stopStream();
-	
+	public void startStream(String url);
+	public void stopStream();
+	public boolean isPerforming();
 }
