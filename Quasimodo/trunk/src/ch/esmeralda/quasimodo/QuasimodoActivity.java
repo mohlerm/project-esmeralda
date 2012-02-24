d9a988dc85f9e2a4982971d641e5f4cab620614e
marcoeppenberger@gmail.com <marcoeppenberger@gmail.com@46f43c3b-6de7-3a5c-3d44-7744824c1714>
2012-02-24 19:11:07 +0000

    first try listview

--- Quasimodo/trunk/src/ch/esmeralda/quasimodo/QuasimodoActivity.java
+++ Quasimodo/trunk/src/ch/esmeralda/quasimodo/QuasimodoActivity.java
@@ -2,12 +2,35 @@ package ch.esmeralda.quasimodo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class QuasimodoActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);   
         setContentView(R.layout.main);
+        
+        
+        final String[] COUNTRIES = new String[] {
+            "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",
+            "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina",
+            "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan",
+            "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
+            "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
+            "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory",
+            "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi",
+            "Cote d'Ivoire", "Cambodia", "Cameroon", "Canada", "Cape Verde"
+          };
+        
+        
+        
+        ListView lv = (ListView) findViewById(R.id.listView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, COUNTRIES);
+        lv.setAdapter(adapter);
+      
+        
     }
 }
