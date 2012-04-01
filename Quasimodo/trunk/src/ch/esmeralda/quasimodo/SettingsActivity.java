581099748424d625e445bb4020a5496257a1549a
marcoeppenberger@gmail.com <marcoeppenberger@gmail.com@46f43c3b-6de7-3a5c-3d44-7744824c1714>
2012-04-01 22:08:31 +0000

    Eppi editActivit so verändert dass basics über die TU eingegeben werden können. (Layout fertig, Backend immernoch todo!) Anpassung der Haupt Activity damit die TU korrekt angezeigt werden.

--- Quasimodo/trunk/src/ch/esmeralda/quasimodo/SettingsActivity.java
+++ Quasimodo/trunk/src/ch/esmeralda/quasimodo/SettingsActivity.java
@@ -13,7 +13,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
