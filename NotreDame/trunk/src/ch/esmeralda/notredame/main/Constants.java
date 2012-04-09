01e3826567010ad61bdefbab14ba7399c25b9965
thomasrichner <thomasrichner@gmail.com>
2012-04-09 10:47:14 +0000

    minor optimizations

--- NotreDame/trunk/src/ch/esmeralda/notredame/main/Constants.java
+++ NotreDame/trunk/src/ch/esmeralda/notredame/main/Constants.java
@@ -2,7 +2,7 @@ package ch.esmeralda.notredame.main;
 
 public class Constants {
 	public static final String VERSION = "1.0";
-	
+	public static final String DI_TRANCE = "http://u11aw.di.fm:80/di_trance";
 	public static boolean V = false;
 	public static boolean I = false;
 	public static boolean D = false;
