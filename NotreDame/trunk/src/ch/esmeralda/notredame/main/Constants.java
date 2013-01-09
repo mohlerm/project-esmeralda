6d8e397388e545058500857850ecf07c490ea093
marcoeppenberger <marcoeppenberger@gmail.com>
2013-01-09 13:52:29 +0000

    DI stream URLs updated

--- NotreDame/trunk/src/ch/esmeralda/notredame/main/Constants.java
+++ NotreDame/trunk/src/ch/esmeralda/notredame/main/Constants.java
@@ -4,8 +4,8 @@ import java.util.TimeZone;
 
 public class Constants {
 	public static final String VERSION = "1.0";
-	public static final String DI_TRANCE = "http://u11aw.di.fm:80/di_trance";
-	public static final String DI_CHIPTUNES = "http://u11aw.di.fm:80/di_chiptunes";
+	public static final String DI_TRANCE = "http://pub3.di.fm:80/di_trance";
+	public static final String DI_CHIPTUNES = "http://pub3.di.fm:80/di_chiptunes";
 	//public static final long UTC_OFFSET = -(TimeZone.getDefault().getDSTSavings() + TimeZone.getDefault().getRawOffset())/(1000*3600);
 	public static final int SERVERPORT = 10002;
 	
